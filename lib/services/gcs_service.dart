e4648205d090a05c433a6ab2228cd8e67254d397
V ravichnadran <vish191988@outlook.com>
2025-05-23 01:39:35 +1000

    Update gcs_service.dart

--- lib/services/gcs_service.dart
+++ lib/services/gcs_service.dart
@@ -1,17 +1,22 @@
-import 'dart:convert';
+import 'dart:typed_data';
 import 'dart:io';
 import 'package:googleapis/storage/v1.dart' as gcs;
 import 'package:googleapis_auth/auth_io.dart';
 
 Future<String> uploadProfileImageToGCS(Uint8List imageBytes, String userId) async {
-  final accountCredentials = ServiceAccountCredentials.fromJson(
-    Platform.environment['587e882b1d3452150b9607941e910c47ca55c26b']!,
-  );
+  final credentialsJson = Platform.environment['GOOGLE_APPLICATION_CREDENTIALS_JSON'];
+  if (credentialsJson == null) {
+    throw Exception('GOOGLE_APPLICATION_CREDENTIALS_JSON not set');
+  }
+  final credentials = ServiceAccountCredentials.fromJson(credentialsJson);
   final scopes = [gcs.StorageApi.devstorageFullControlScope];
-  final client = await clientViaServiceAccount(accountCredentials, scopes);
+  final client = await clientViaServiceAccount(credentials, scopes);
 
   final storage = gcs.StorageApi(client);
-  final bucket = Platform.environment['photoaid17']!;
+  final bucket = Platform.environment['GCS_BUCKET'];
+  if (bucket == null) {
+    throw Exception('GCS_BUCKET not set');
+  }
 
   final objectName = 'profile_images/$userId.jpg';
   final media = gcs.Media(Stream.value(imageBytes), imageBytes.length, contentType: 'image/jpeg');
@@ -26,4 +31,3 @@ Future<String> uploadProfileImageToGCS(Uint8List imageBytes, String userId) asyn
   // Public URL (if bucket/object is public)
   return 'https://storage.googleapis.com/$bucket/$objectName';
 }
-
